5c2a912e95c9e013a46fcbf519212f6cb51ca65a
narekhovhannisyan <narekgilmour@gmail.com>
2017-11-12 00:40:31 +0400

    src: in Program.cs extracted Node class from linked-list class
     - added temp node
     - getHead function renamed to getFirst
     - getTail renamed to getLast
     - implemented add method
     - implemented search method
     - implemented remove method
     - changed readme.md

--- src/Program.cs
+++ src/Program.cs
@@ -1,34 +1,55 @@
 ﻿using System;
 
 namespace linked_list {
-    class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello World!");
-        }
-    }
-    class linkedList {
-        class Node {
-            public Node Next;
-            public Node Prev;
-            public int Value;
-        }
+	class Program {
+		static void Main (string[] args) { }
+	}
 
-        Node head, tail = null;
-        linkedList () {
-            head.Next = tail;
-            head.Prev = null;
-            tail.Prev = tail;
-            tail.Next = null;
-        }
+	class Node {
+		public Node Next;
+		public Node Prev;
+		public int Value;
+	}
 
-        public int getHead () {
-            if (head != null) return head.Value;
-            else throw new ArgumentNullException ("Head is null.");
-        }
+	class linkedList {
 
-        public int getTail () {
-            if (tail != null) return tail.Value;
-            else throw new ArgumentNullException ("Tail is null.");
-        }
-    }
+		Node head, tail, temp = null;
+
+		public int getFirst () {
+			if (head != null) return head.Value;
+			else throw new ArgumentNullException ("Head is null.");
+		}
+
+		public int getLast () {
+			if (tail != null) return tail.Value;
+			else throw new ArgumentNullException ("Tail is null.");
+		}
+
+		public void add (int value) {
+			if (head == tail) {
+				head.Value = tail.Value = value;
+				head.Next = tail.Prev;
+				head.Prev = tail.Next = null;
+			} else {
+				temp = head;
+				head.Value = value;
+				head.Next = temp;
+			}
+		}
+
+		public Node search (int value) {
+			temp = head;
+			while (temp != null && temp.Value != value) {
+				temp = temp.Next;
+			}
+			return temp;
+		}
+
+		public void remove (int value) {
+			temp = search (value);
+			if (temp.Prev != null) temp.Prev.Next = temp.Next;
+			else head = temp.Next;
+			if (temp.Next != null) temp.Next.Prev = temp.Prev;
+		}
+	}
 }
