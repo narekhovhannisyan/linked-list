3c19b6feaae9404fcd77cec4047ce3912a2e5c7a
narekhovhannisyan <narekgilmour@gmail.com>
2017-11-11 20:18:41 +0400

    root: moved Program.cs to src directory
     - updated readme.md file
     - implemented linked list class with node
       and getHead/getTail methods

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # linked-list
+
+A linked list is a data structure in which the objects are arranged in a linear order.
+
+Each element of a `doubly linked list` `L` is an object with
+an attribute key and two other pointer attributes:
+
+`next` and `prev`. Given an element x in the list, `x.next` points to its successor in the linked list, and `x.prev` points to its predecessor. If `x.prev` is NIL, the element `x` has no predecessor and is therefore the first element, or head, of
+the list. If x:next = NIL, the element `x` has no successor and is therefore the last
+element, or tail, of the list. An attribute `L.head` points to the first element of the
+list. If `L.head` = NIL, the list is empty.
+
---
+++ src/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace linked_list {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+    class linkedList {
+        class Node {
+            public Node Next;
+            public Node Prev;
+            public int Value;
+        }
+
+        Node head, tail = null;
+        linkedList () {
+            head.Next = tail;
+            head.Prev = null;
+            tail.Prev = tail;
+            tail.Next = null;
+        }
+
+        public int getHead () {
+            if (head != null) return head.Value;
+            else throw new ArgumentNullException ("Head is null.");
+        }
+
+        public int getTail () {
+            if (tail != null) return tail.Value;
+            else throw new ArgumentNullException ("Tail is null.");
+        }
+    }
+}
